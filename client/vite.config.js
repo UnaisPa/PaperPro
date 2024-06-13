9cf82ff52342b989dfa548662753296c1cda2393
Unais P A <unaismuhmed712@gmail.com>
2024-06-13 10:32:40 +0530

    vite config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:5173,
+  server: {
+    port: 5173,
     proxy: {
       '/api': {
-        target:'http://localhost:5000',
-        changeOrigin:true
-      }
-    },
-    build: {
-        // generate .vite/manifest.json in outDir
-        manifest: true,
-        rollupOptions: {
-          // overwrite default .html entry
-          input: '/src/main.jsx',
-        },
+        target: 'http://localhost:5000',
+        changeOrigin: true,
       },
-    host:true
+    },
+    host: true,
   },
-  resolve:{
-    alias:{
-        "@": path.resolve(__dirname,"./src")
-    }
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: path.resolve(__dirname, 'src/main.jsx'),
+    },
   },
-  
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
